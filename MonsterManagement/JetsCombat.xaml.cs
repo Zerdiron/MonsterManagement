4b282976076e0d7f8d2e12ae5724d7f2ca9941e8
Zerdiron <erdiron@hotmail.fr>
2017-03-09 16:24:07 +0100

    Interface en cours de mise à jour

--- MonsterManagement/JetsCombat.xaml.cs
+++ MonsterManagement/JetsCombat.xaml.cs
@@ -10,42 +10,42 @@ namespace MonsterManagement
 	/// </summary>
 	public partial class JetsCombat : Page
 	{
-		
+
 		/// <summary>
 		/// Le tableau de stats de l'invocation.
 		/// </summary>
 		private short[] _Caracs;
-		
+
 		/// <summary>
 		/// Le tableau des jets de sauvegardes de l'invocation.
 		/// </summary>
 		private short[] _JDS;
-		
+
 		/// <summary>
 		/// Le tableau des stats de la première attaque de l'invocation.
 		/// </summary>
 		private short[] _AttaqueUn;
-		
+
 		/// <summary>
 		/// Le tableau des stats de la seconde attaque de l'invocation.
 		/// </summary>
 		private short[] _AttaqueDeux;
-		
+
 		/// <summary>
 		/// Le tableau des stats de la troisième attaque de l'invocation.
 		/// </summary>
 		private short[] _AttaqueTrois;
-		
+
 		/// <summary>
 		/// Nombre d'invocation invoqué déterminé par le FP et le Level du sort.
 		/// </summary>
 		private int _nombreInvoc;
-		
+
 		/// <summary>
 		/// 
 		/// </summary>
 		private Label[] TabInvoc;
-		
+
 		/// <summary>
 		/// Si l'invocation joue avec avantage ou non à ses jets.
 		/// </summary>
@@ -69,12 +69,12 @@ namespace MonsterManagement
 		{
 			// On récupère les variables transmises au constructeur.
 			_Caracs = Caracs; _JDS = JDS;
-			_AttaqueUn = AttaqueUn;	_AttaqueDeux = AttaqueDeux;	_AttaqueTrois = AttaqueTrois;
+			_AttaqueUn = AttaqueUn; _AttaqueDeux = AttaqueDeux; _AttaqueTrois = AttaqueTrois;
 			_avantage = avantage;
 			_nombreInvoc = NombreInvoc;
 
 			InitializeComponent();
-			
+
 			TabInvoc = new Label[NombreInvoc];
 
 			// On définit la taille des colonnes.
@@ -103,145 +103,154 @@ namespace MonsterManagement
 				JetCombat.RowDefinitions.Add(row);
 
 				Label label = new Label();
-				if (i % 2 == 0) { label.Background = Brushes.Coral; label.Foreground = Brushes.Black; }
-				else { label.Background = Brushes.LightSteelBlue; label.Foreground = Brushes.Black; }
+				if (i % 2 == 0) label.Background = Brushes.Coral;
+				else label.Background = Brushes.LightSteelBlue;
+				label.Foreground = Brushes.Black;
 				Grid.SetRow(label, i + k); Grid.SetColumn(label, j);
 				JetCombat.Children.Add(label);
 				TabInvoc[i - 1] = label;
 
-				if (i == NombreInvoc)
-				{
-					Button refresh = new Button();
-					refresh.HorizontalAlignment = HorizontalAlignment.Center;
-					refresh.VerticalAlignment = VerticalAlignment.Center;
-					refresh.Width = 150;
-					refresh.Click += Refresh_Click;
-					refresh.Content = "Refresh";
-					Grid.SetColumn(refresh, 0);
-					Grid.SetRow(refresh, 1);
-					Button.Children.Add(refresh);
-				}
-
 				if (i > 1 && i % 16 == 0) { j++; k -= 16; }
 			}
+
+			Button JetAttaque = new Button();
+			JetAttaque.Name = "JetAttaque";
+			JetAttaque.HorizontalAlignment = HorizontalAlignment.Center;
+			JetAttaque.VerticalAlignment = VerticalAlignment.Center;
+			JetAttaque.Margin = new Thickness(200, 0, 0, 0);
+			JetAttaque.Width = 150;
+			JetAttaque.Click += Refresh_Click;
+			JetAttaque.Content = "Jets d'attaque";
+			Grid.SetColumn(JetAttaque, 0);
+			Grid.SetRow(JetAttaque, 1);
+			Button.Children.Add(JetAttaque);
+
+			Button JetSauvegarde = new Button();
+			JetSauvegarde.Name = "JetSauvegarde";
+			JetSauvegarde.HorizontalAlignment = HorizontalAlignment.Center;
+			JetSauvegarde.VerticalAlignment = VerticalAlignment.Center;
+			JetSauvegarde.Margin = new Thickness(0, 0, 200, 0);
+			JetSauvegarde.Width = 150;
+			JetSauvegarde.Click += Refresh_Click;
+			JetSauvegarde.Content = "Jets de Sauvegarde";
+			Grid.SetColumn(JetSauvegarde, 0);
+			Grid.SetRow(JetSauvegarde, 1);
+			Button.Children.Add(JetSauvegarde);
 		}
 
 		private void Refresh_Click(object sender, RoutedEventArgs e)
 		{
-			if (_AttaqueUn[2] > 0)
+			if (sender.Equals(JetAttaque))
 			{
-				for (short i = 0; i < _nombreInvoc; i++)
-				{
-					int d20 = Randomizer.Next(1, 21);
-					int jetAttaqueNaturel = d20;
 
-					if (_avantage)
+				if (_AttaqueUn[2] > 0)
+					for (short i = 0; i < _nombreInvoc; i++)
 					{
-						int d20_2 = Randomizer.Next(1, 21);
-						if (d20_2 > d20)
-							jetAttaqueNaturel = d20_2;
-						else
-							jetAttaqueNaturel = d20;
+						int d20 = Randomizer.Next(1, 21);
+						int jetAttaqueNaturel = d20;
+
+						if (_avantage)
+						{
+							int d20_2 = Randomizer.Next(1, 21);
+							if (d20_2 > d20)
+								jetAttaqueNaturel = d20_2;
+							else
+								jetAttaqueNaturel = d20;
+						}
+
+						int nombreDe = _AttaqueUn[1];
+						string crit = "";
+
+						if (jetAttaqueNaturel == 20)
+						{
+							nombreDe = nombreDe * 2;
+							crit = "(crit)";
+						}
+
+						int totalDegat = 0;
+
+						for (short j = 0; j < nombreDe; j++)
+							totalDegat += Randomizer.Next(1, _AttaqueUn[2] + 1);
+
+						totalDegat += _AttaqueUn[3];
+
+						int jetAttaque = jetAttaqueNaturel + _AttaqueUn[0];
+
+						TabInvoc[i].Content = string.Format("Invoc {0}: {1}|{2}{3}", i + 1, jetAttaque, totalDegat, crit);
 					}
 
-					int nombreDe = _AttaqueUn[1];
-					string crit = "";
-
-					if (jetAttaqueNaturel == 20)
+				if (_AttaqueDeux[2] > 0)
+					for (short i = 0; i < _nombreInvoc; i++)
 					{
-						nombreDe = nombreDe * 2;
-						crit = "(crit)";
+						int d20 = Randomizer.Next(1, 21);
+						int jetAttaqueNaturel = d20;
+
+						if (_avantage)
+						{
+							int d20_2 = Randomizer.Next(1, 21);
+							if (d20_2 > d20)
+								jetAttaqueNaturel = d20_2;
+							else
+								jetAttaqueNaturel = d20;
+						}
+
+						int nombreDe = _AttaqueUn[1];
+						string crit = "";
+
+						if (jetAttaqueNaturel == 20)
+						{
+							nombreDe = nombreDe * 2;
+							crit = "(crit)";
+						}
+
+						int totalDegat = 0;
+
+						for (short j = 0; j < nombreDe; j++)
+							totalDegat += Randomizer.Next(1, _AttaqueUn[2] + 1);
+
+						totalDegat += _AttaqueUn[3];
+
+						int jetAttaque = jetAttaqueNaturel + _AttaqueUn[0];
+
+						TabInvoc[i].Content += string.Format(", {1}|{2}{3}", i + 1, jetAttaque, totalDegat, crit);
 					}
 
-					int totalDegat = 0;
+				if (_AttaqueTrois[2] > 0)
+					for (short i = 0; i < _nombreInvoc; i++)
+					{
+						int d20 = Randomizer.Next(1, 21);
+						int jetAttaqueNaturel = d20;
 
-					for (short j = 0; j < nombreDe; j++)
-						totalDegat += Randomizer.Next(1, _AttaqueUn[2] + 1);
+						if (_avantage)
+						{
+							int d20_2 = Randomizer.Next(1, 21);
+							if (d20_2 > d20)
+								jetAttaqueNaturel = d20_2;
+							else
+								jetAttaqueNaturel = d20;
+						}
 
-					totalDegat += _AttaqueUn[3];
+						int nombreDe = _AttaqueUn[1];
+						string crit = "";
 
-					int jetAttaque = jetAttaqueNaturel + _AttaqueUn[0];
+						if (jetAttaqueNaturel == 20)
+						{
+							nombreDe = nombreDe * 2;
+							crit = "(crit)";
+						}
 
-					TabInvoc[i].Content = string.Format("Invoc {0}: {1}|{2}{3}", i + 1, jetAttaque, totalDegat, crit);
-				}
+						int totalDegat = 0;
+
+						for (short j = 0; j < nombreDe; j++)
+							totalDegat += Randomizer.Next(1, _AttaqueUn[2] + 1);
+
+						totalDegat += _AttaqueUn[3];
+
+						int jetAttaque = jetAttaqueNaturel + _AttaqueUn[0];
+
+						TabInvoc[i].Content += string.Format(", {1}|{2}{3}", i + 1, jetAttaque, totalDegat, crit);
+					}
 			}
-
-			if (_AttaqueDeux[2] > 0)
-			{
-				for (short i = 0; i < _nombreInvoc; i++)
-				{
-					int d20 = Randomizer.Next(1, 21);
-					int jetAttaqueNaturel = d20;
-
-					if (_avantage)
-					{
-						int d20_2 = Randomizer.Next(1, 21);
-						if (d20_2 > d20)
-							jetAttaqueNaturel = d20_2;
-						else
-							jetAttaqueNaturel = d20;
-					}
-
-					int nombreDe = _AttaqueUn[1];
-					string crit = "";
-
-					if (jetAttaqueNaturel == 20)
-					{
-						nombreDe = nombreDe * 2;
-						crit = "(crit)";
-					}
-
-					int totalDegat = 0;
-
-					for (short j = 0; j < nombreDe; j++)
-						totalDegat += Randomizer.Next(1, _AttaqueUn[2] + 1);
-
-					totalDegat += _AttaqueUn[3];
-
-					int jetAttaque = jetAttaqueNaturel + _AttaqueUn[0];
-
-					TabInvoc[i].Content += string.Format(", {1}|{2}{3}", i + 1, jetAttaque, totalDegat, crit);
-				}
-			}
-
-			if (_AttaqueTrois[2] > 0)
-			{
-				for (short i = 0; i < _nombreInvoc; i++)
-				{
-					int d20 = Randomizer.Next(1, 21);
-					int jetAttaqueNaturel = d20;
-
-					if (_avantage)
-					{
-						int d20_2 = Randomizer.Next(1, 21);
-						if (d20_2 > d20)
-							jetAttaqueNaturel = d20_2;
-						else
-							jetAttaqueNaturel = d20;
-					}
-
-					int nombreDe = _AttaqueUn[1];
-					string crit = "";
-
-					if (jetAttaqueNaturel == 20)
-					{
-						nombreDe = nombreDe * 2;
-						crit = "(crit)";
-					}
-
-					int totalDegat = 0;
-
-					for (short j = 0; j < nombreDe; j++)
-						totalDegat += Randomizer.Next(1, _AttaqueUn[2] + 1);
-
-					totalDegat += _AttaqueUn[3];
-
-					int jetAttaque = jetAttaqueNaturel + _AttaqueUn[0];
-
-					TabInvoc[i].Content += string.Format(", {1}|{2}{3}", i + 1, jetAttaque, totalDegat, crit);
-				}
-			}
-
 		}
 	}
 }
